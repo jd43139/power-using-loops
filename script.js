b90c5cf4bc0ad96eb630df33d4c64e9e23fcd7f3
Maher <109301496+jd43139@users.noreply.github.com>
2023-02-12 09:19:44 +0400

    Add .

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function pow(x,n){
+/**This is my solution  */
+/*function pow(x,n){
     let number=x
     let number2=x
     for(let i=0; i<n; i++){
@@ -8,13 +9,66 @@ function pow(x,n){
     }
     return number2/x
     
+  }*/
+  /*function showErrorMessage(){
+    alert("Please Enter Only Numbers From 0.")
+  }
+
+  function pow(x,n){
+    let number_1=x
+    let number_2=x
+    while(n>1){
+      result*=x
+      n--
+    }
+    
+  
+    return (n==0)? 1 : number_1
+  }
+  function validateInput(num){
+    if(typeof num==="number" && num>=0){
+      return true
+    }else{
+      return false
+    }
+    
   }
 
 
   let number=Number(prompt("Enter a number",7))
   if(number){
     let times=Number(prompt("Enter the power"))
-    if(times){
+    if(validateInput(times)){
         alert(pow(number,times))
+    }else{
+      showErrorMessage()
+      
     }
+    
+  }else{
+    showErrorMessage()
+  }*/
+/**This is the real solution from javascript.info */
+
+function pow(x,n){
+  let result=x
+  for(let i=1; i<n; i++){
+    result*=x
   }
+  return result
+}
+
+let number=Number(prompt("number?",3))
+
+if (number){
+  let power=Number(prompt("power?",2))
+  if(power && power>=0){
+    alert(pow(number,power))
+  }else{
+    alert("Enter Numbers Only Equal or greater then 0.")
+
+  }
+  
+}else{
+  alert("Enter Numbers Only")
+}
